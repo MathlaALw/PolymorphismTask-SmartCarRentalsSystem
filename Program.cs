5bfaa05e4c442c74ad115bd5df9148ee8cc8cded
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-07-23 11:07:20 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PolymorphismTask_SmartCarRentalsSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
